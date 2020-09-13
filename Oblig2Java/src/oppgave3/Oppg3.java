8fad26c312e80cbe90cb220b1a1ff5c3ac7ed15f
olive <olive@DESKTOP-R044BGP>
2020-09-13 21:42:34 +0200

    final

--- Oblig2Java/src/oppgave3/Oppg3.java
+++ Oblig2Java/src/oppgave3/Oppg3.java
@@ -1,6 +1,7 @@
 package oppgave3;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
